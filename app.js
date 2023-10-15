aa5243de0f2e6b43661969045a4e6a3f235c5e58
michaloumen <michelle.mendonca@minu.co>
2023-10-15 15:53:23 -0300

    connecting to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+// app
+const app = express();
+
+// database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Database connected');
+}).catch((error) => {
+  console.error('Error connecting to the database:', error);
+});
+
+// routes
 app.get('/', (req, res) => {
   res.send('hello from nodeeee');
 });
@@ -10,4 +24,4 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}); 
+});
