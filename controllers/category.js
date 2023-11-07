332e35a0fa95e330f8daf85b63b24d78af674f49
michaloumen <michelle.mendonca@minu.co>
2023-11-07 08:11:55 -0300

    category by id and single category

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,24 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.categoryById = (req, res, next, categoryId) => {
+  Category.findById(categoryId)
+    .then((category) => {
+      // if (!category) {
+      //   return res.status(400).json({
+      //     error: 'Category does not exist'
+      //   });
+      // }
+      req.category = category;
+      next();
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        error: 'Category does not exist'
+      });
+    });
+};
+
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save()
@@ -12,4 +30,8 @@ exports.create = (req, res) => {
         error: errorHandler(err)
       });
     });
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
 };
