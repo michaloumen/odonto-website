b43fdad4a7d64e627efbf464540f54dab0be9303
michaloumen <michelle.mendonca@minu.co>
2023-11-06 21:36:10 -0300

    product by id middleware and single product

--- controllers/product.js
+++ controllers/product.js
@@ -6,11 +6,42 @@ const storage = multer.memoryStorage(); // Armazena a imagem na memória
 
 const upload = multer({ storage: storage }).single('photo');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id)
+  .then((product) => {
+    if (!product) {
+      return res.status(400).json({
+        error: 'Product not found'
+      });
+    }
+    req.product = product;
+    next();
+  })
+  .catch((err) => {
+    return res.status(400).json({
+      error: 'Error while fetching the product'
+    });
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   upload(req, res, (err) => {
     if (err) {
       return res.status(400).json({
         error: 'Image could not be uploaded'
+      });
+    }
+
+    const fields = req.body;
+    const { name, description, price, category, quantity } = fields;
+    if (!name || !description || !price || !category || !quantity) {
+      return res.status(400).json({
+        error: 'All fields are required',
       });
     }
 
