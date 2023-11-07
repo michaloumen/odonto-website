0ba8e1d968661ec68dd5cd35f3e73f9fe117c346
michaloumen <michelle.mendonca@minu.co>
2023-11-07 08:36:56 -0300

    category update delete and get all

--- controllers/category.js
+++ controllers/category.js
@@ -35,3 +35,50 @@ exports.create = (req, res) => {
 exports.read = (req, res) => {
   return res.json(req.category);
 };
+
+exports.update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  
+  category.save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(400).json({
+        error: errorHandler(err)
+      });
+    });
+};
+
+exports.remove = (req, res) => {
+  const category = req.category;
+
+  if (!category) {
+    return res.status(404).json({
+      error: 'Category not found'
+    });
+  }
+
+  category.deleteOne()
+    .then(data => {
+      res.json({ message: 'Category deleted' });
+    })
+    .catch(err => {
+      res.status(400).json({
+        error: errorHandler(err)
+      });
+    });
+};
+
+exports.list = (req, res) => {
+  Category.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(400).json({
+        error: errorHandler(err)
+      });
+    });
+};
