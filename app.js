eb2f75fe1009e06bb0d6934b1fc60b4e72734fdc
michaloumen <michelle.mendonca@minu.co>
2024-03-05 15:03:18 -0300

    mongodb update and fixes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { body, validationResult } = require('express-validator');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
@@ -24,14 +25,20 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // middlewares
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(expressValidator());
-app.use(cors());
 
+// express-validator middleware
+app.use(
+  express.json(),
+  express.urlencoded({ extended: true }),
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 }),
+);
 
-// routes
+// Routes
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
@@ -42,3 +49,4 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
